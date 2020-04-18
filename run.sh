2644c56e7f7cc26652457c52dce26e40d4475de0
garyzcc <33635035+ChichengZhang@users.noreply.github.com>
2020-04-17 20:34:24 -0400

    3.7

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 # Use this shell script to compile (if necessary) your code and then execute it. Belw is an example of what might be found in this file if your program was written in Python 3.7
 
 # Test Code 1
-# python ./src/consumer_complaints.py ./insight_testsuite/test_1/input/complaints.csv ./insight_testsuite/test_1/output/report.csv
+# python3.7 ./src/consumer_complaints.py ./insight_testsuite/test_1/input/complaints.csv
+# ./insight_testsuite/test_1/output/report.csv
 
  # Test Code 2
-# python ./src/consumer_complaints.py ./insight_testsuite/test_2/input/complaints.csv ./insight_testsuite/test_2/output/report.csv
+# python3.7 ./src/consumer_complaints.py ./insight_testsuite/test_2/input/complaints.csv
+# ./insight_testsuite/test_2/output/report.csv
 
 # Fianl Code
- python ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
+ python3.7 ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
