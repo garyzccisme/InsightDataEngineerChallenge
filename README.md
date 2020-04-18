afa4fac8c47c96c0f85a71feb07acab54c3c35c8
garyzcc <33635035+ChichengZhang@users.noreply.github.com>
2020-04-17 20:07:32 -0400

    final

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # InsightDataEngineerChallenge
+
+## Summary
+My solution's logic is very simple and clear, which can be find at `./src/solution.py`. 
+The key point is to get the primary key of output table, which is (Product, Year).
+Since third party packages are not allowed here, I adopt `Dictionary` data structure to deal with the data manipulation.
+Other explanations can be found in the code docstrings and comments.
+
+## Run Instruction
+In the `run.sh` file, there are totally three code lines: 
+- Test Code 1 (Commented), the test dataset (small size) is provided by default repo.
+- Test Code 2 (Commented), the test dataset (modest size) is provided by original repo's `README.md`. While the file
+ is too large to push to repo, so I cut it up and push a subset into `test_2` folder. The original dataset can be
+  find at [Here](http://files.consumerfinance.gov/ccdb/complaints.csv.zip).
+- Final Code (Uncommented), the practical code for evaluation. Please make sure you create the `complaints.csv` in
+ `input` folder. Otherwise it will come out with file not found Error.
+ 
+ ## Improvement
+ When running `_initialize()` method for `Solution` class, we can design a Heap data structure for company counts
+  instead of store all companies counts in extra dictionary then find max count.  
--- src/solution.py
+++ src/solution.py
@@ -10,9 +10,9 @@ class Solution:
         self.write_path = write_path
 
         # Define some intermediate variables
-        self.raw = {}
-        self.mid = {}
-        self.final = None
+        self._raw = {}
+        self._mid = {}
+        self._final = None
 
     def pipeline(self):
         """
@@ -26,78 +26,78 @@ class Solution:
 
     def _read_csv(self):
         """
-        Read csv file, filter out unrelated columns and transfer the data into dictionary, stored in self.raw.
+        Read csv file, filter out unrelated columns and transfer the data into dictionary, stored in self._raw.
         Schema: {Complaint ID: [Product, Date received, Company]}
         """
         with open(self.read_path, 'r', encoding='UTF-8') as file:
-            csv_file = csv.DictReader(file)
+            csv_reader = csv.DictReader(file)
             keys = ['Complaint ID', 'Product', 'Date received', 'Company']
-            for i, row in enumerate(csv_file):
+            for i, row in enumerate(csv_reader):
                 tem_dict = dict(row)
                 record = [tem_dict.get(x) for x in keys]
 
                 # Check Validation
                 if not all(record):
                     continue
-                self.raw[record[0]] = list(map(str.lower, record[1:]))
+                self._raw[record[0]] = list(map(str.lower, record[1:]))
 
     # TODO: Could use HEAP data structure here to gain more efficiency
     def _initialize(self):
         """
-        Get primary key (Product, Year), set it as dictionary key to generate new dictionary, stored in self.mid.
+        Get primary key (Product, Year), set it as dictionary key to generate new dictionary, stored in self._mid.
         Schema: {(Product, Year): [Complaints_number, Complaints_company_number, dict(company, counts)]}
         """
-        if self.raw == {}:
+        if self._raw == {}:
             raise SyntaxError("Invalid Syntax: self.raw is not initialized.")
 
-        for index, record in self.raw.items():
+        for index, record in self._raw.items():
 
             # (Product, Year) should be output table's primary key
             pk = (record[0], datetime.strptime(record[1], '%Y-%m-%d').year)
-            if pk not in self.mid:
+            if pk not in self._mid:
                 complaint_counts = 1
                 company_counts = 1
                 company = {record[2]: 1}
-                self.mid[pk] = [complaint_counts, company_counts, company]
+                self._mid[pk] = [complaint_counts, company_counts, company]
             else:
-                self.mid[pk][0] += 1
+                self._mid[pk][0] += 1
 
                 # Check if the company has shown before
-                if record[2] not in self.mid[pk][2]:
-                    self.mid[pk][1] += 1
-                    self.mid[pk][2][record[2]] = 1
+                if record[2] not in self._mid[pk][2]:
+                    self._mid[pk][1] += 1
+                    self._mid[pk][2][record[2]] = 1
                 else:
-                    self.mid[pk][2][record[2]] += 1
+                    self._mid[pk][2][record[2]] += 1
 
     def _process(self):
         """
-        Focus on the dict(company, counts) in self.mid, get max value and calculate percentage, updated in self.mid.
+        Focus on the dict(company, counts) in self._mid, get max value and calculate percentage, updated in self._mid.
         Schema: {(Product, Year): [Complaints_number, Complaints_company_number, max_complaints_percent_by_company]}
         """
-        if self.mid == {}:
+        if self._mid == {}:
             raise SyntaxError("Invalid Syntax: self.mid is not initialized.")
-        items = list(self.mid.items())
+        items = list(self._mid.items())
         for pk, target in items:
             max_percent = int(round(max(target[-1].values()) / target[0] * 100, 0))
-            self.mid[pk][-1] = max_percent
+            self._mid[pk][-1] = max_percent
 
     def _sort(self):
         """
-        Reconstruct self.mid into list, sort it by primary key, stored in self.final.
+        Reconstruct self._mid into list, sort it by primary key, stored in self._final.
         Schema: [[Product, Year, Complaints_number, Complaints_company_number, max_complaints_percent_by_company]]
         """
-        if self.mid == {}:
+        if self._mid == {}:
             raise SyntaxError("Invalid Syntax: self.mid is not initialized.")
-        self.final = [[*pk, *target] for pk, target in self.mid.items()]
-        self.final.sort(key=lambda x: (x[0], x[1]))
+        self._final = [[*pk, *target] for pk, target in self._mid.items()]
+        self._final.sort(key=lambda x: (x[0], x[1]))
 
     def _write_csv(self):
         """
-        Write self.final into csv file.
+        Write self._final into csv file.
         """
         with open(self.write_path, mode='w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            for row in self.final:
+            for row in self._final:
                 csv_writer.writerow(row)
 
 
